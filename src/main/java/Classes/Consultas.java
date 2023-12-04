c0afb92eacd1d819896907e2efd994f856a160a1
Jose Vargas <129638981+josepavaa118@users.noreply.github.com>
2023-12-04 13:31:51 -0600

    Improved Query Logic
    Added logic to show winner according to the given search criteria (name, first/last name)

--- src/main/java/Classes/Consultas.java
+++ src/main/java/Classes/Consultas.java
@@ -20,51 +20,105 @@ public class Consultas {
     }
     
     public void consultasNombre(String nombre){
+        int[] resultados;
+        resultados= new int[partidosCompleto.length];
         String datos="";
+        String ganador="";
         String nombreFormato=nombre.toUpperCase();
+        int indice=0;
         for (int x=0;x<=padronCompleto.length-1;x++){
             if (padronCompleto[x][5].contains(nombreFormato)){
                 datos=datos+"\n"+"La Persona "+padronCompleto[x][5]+" ha votado por el Partido: "+padronCompleto[x][8]+"\n";
+                indice=asignarVotos(padronCompleto[x][8]);
+                resultados[indice]=resultados[indice]+1;
             }
         }
         if(datos.isEmpty()){
             JOptionPane.showMessageDialog(null, "Lo sentimos. No hay registro de votos con el nombre dado"); 
         }
         else{
+            ganador=declararGanador(resultados);
             JOptionPane.showMessageDialog(null, datos);
+            JOptionPane.showMessageDialog(null, ganador);
         }
     }
     
     public void consultasApellido1(String apellido){
+        int[] resultados;
+        resultados= new int[partidosCompleto.length];
         String datos="";
+        String ganador="";
+        int indice=0;
         String apellidoFormato=apellido.toUpperCase();
         for (int x=0;x<=padronCompleto.length-1;x++){
             if (padronCompleto[x][6].contains(apellidoFormato)){
                 datos=datos+"\n"+"La Persona "+padronCompleto[x][5]+padronCompleto[x][6]+" ha votado por el Partido: "+padronCompleto[x][8]+"\n";
+                indice=asignarVotos(padronCompleto[x][8]);
+                resultados[indice]=resultados[indice]+1;
             }
         }
         if(datos.isEmpty()){
             JOptionPane.showMessageDialog(null, "Lo sentimos. No hay registro de votos con el nombre dado"); 
         }
         else{
+            ganador=declararGanador(resultados);
             JOptionPane.showMessageDialog(null, datos);
+            JOptionPane.showMessageDialog(null, ganador);
+            
         }
     }
     
         public void consultasApellido2(String apellido){
+        int[] resultados;
+        resultados= new int[partidosCompleto.length];
         String datos="";
+        String ganador="";
         String apellidoFormato=apellido.toUpperCase();
+        int indice=0;
         for (int x=0;x<=padronCompleto.length-1;x++){
             if (padronCompleto[x][7].contains(apellidoFormato)){
                 datos=datos+"\n"+"La Persona "+padronCompleto[x][5]+padronCompleto[x][6]+ padronCompleto[x][7]+" ha votado por el Partido: "+padronCompleto[x][8]+"\n";
+                indice=asignarVotos(padronCompleto[x][8]);
+                resultados[indice]=resultados[indice]+1;
             }
         }
         if(datos.isEmpty()){
             JOptionPane.showMessageDialog(null, "Lo sentimos. No hay registro de votos con el nombre dado"); 
         }
         else{
+            ganador=declararGanador(resultados);
             JOptionPane.showMessageDialog(null, datos);
+            JOptionPane.showMessageDialog(null, ganador);
         }
     }
+        
+        private int asignarVotos(String partido){
+            int indice=0;
+            for (int x=0;x<=partidosCompleto.length-1;x++){
+                if (partido.equals(partidosCompleto[x][0])){
+                    indice=x;
+                    break;
+                }
+            }
+            return indice;
+        }
+        
+        private String declararGanador(int[] votos){
+            int max=0;
+            int indice=0;
+            String msg="";
+            for (int x=0;x<=votos.length-1;x++){
+                if(max<votos[x]){
+                    max=votos[x];
+                    indice=x;
+                    msg="El partido ganador de acuerdo a esta busqueda es: "+ partidosCompleto[indice][0]+ " con "+max+ " votos";
+                }
+                else if(max==votos[x]){
+                    msg="Hay un empate entre 2 o más partidos";
+                }
+            }
+            
+            return msg;
+        }
     
 }
